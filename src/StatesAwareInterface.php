d041adbde5a28fe4315be437ff2f5a173ecc4cb1
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-25 13:23:40 +0100

    Added `StatesAwareInterface`

---
+++ src/StatesAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dhii\Machine;
+
+use Dhii\Data\ValueAwareInterface;
+use Traversable;
+
+/**
+ * Something which has a set of states..
+ *
+ * @since [*next-version*]
+ */
+interface StatesAwareInterface
+{
+    /**
+     * Retrieves the states.
+     *
+     * @since [*next-version*]
+     *
+     * @return ValueAwareInterface[]|Traversable The collection of states.
+     */
+    public function getStates();
+}
