794618432ac9b4739be41f32daf992b3c1782d21
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-25 21:30:16 +0100

    Added `TransitionExceptionInterface#getTransition()`

--- src/TransitionExceptionInterface.php
+++ src/TransitionExceptionInterface.php
@@ -9,4 +9,12 @@ namespace Dhii\Machine;
  */
 interface TransitionExceptionInterface extends StateMachineExceptionInterface
 {
+    /**
+     * Retrieves the transition that triggered this exception.
+     *
+     * @since [*next-version*]
+     *
+     * @return ValueAwareInterface|null The transition.
+     */
+    public function getTransition();
 }
