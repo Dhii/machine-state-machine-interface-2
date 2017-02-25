18176dba07c715c495521f681a681f46459c383c
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-25 14:28:41 +0100

    Added `TransitionExceptionInterface`

---
+++ src/TransitionExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Machine;
+
+/**
+ * Something that represents an exception thrown during a state transition.
+ *
+ * @since [*next-version*]
+ */
+interface TransitionExceptionInterface extends StateMachineExceptionInterface
+{
+}
---
+++ test/functional/TransitionExceptionInterfaceTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Dhii\Machine\FuncTest;
+
+use Dhii\Machine\TransitionExceptionInterface;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see Dhii\Machine\TransitionExceptionInterface}.
+ *
+ * @since [*next-version*]
+ */
+class TransitionExceptionInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\\Machine\\TransitionExceptionInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TransitionExceptionInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME, $subject, 'Subject is not a valid instance.'
+        );
+        $this->assertInstanceOf(
+            'Dhii\\Machine\\StateMachineExceptionInterface',
+            $subject, 'Subject is not a valid StateMachineExceptionInterface instance.'
+        );
+    }
+}
