1548c0ae74ad6bf07210859604993632f0523aa3
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-25 17:02:41 +0100

    Updated comment docs for `TransitionsAwareInterface`

--- src/TransitionsAwareInterface.php
+++ src/TransitionsAwareInterface.php
@@ -6,18 +6,18 @@ use Dhii\Data\ValueAwareInterface;
 use Traversable;
 
 /**
- * Something that is aware of a set of transitions that can be applied.
+ * Something that is aware of a set of transitions.
  *
  * @since [*next-version*]
  */
 interface TransitionsAwareInterface
 {
     /**
-     * Retrieves the transitions that can be applied.
+     * Retrieves the set of transitions.
      *
      * @since [*next-version*]
      *
-     * @return ValueAwareInterface[]|Traversable A collection of the transition instances.
+     * @return ValueAwareInterface[]|Traversable The set of the transitions.
      */
     public function getTransitions();
 }
